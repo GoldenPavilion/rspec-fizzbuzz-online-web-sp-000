ccf621d5bc82a644a20d435ab1f25b3537990b61
Robert Stine <rcorystine@gmail.com>
2020-12-09 15:46:04 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,6 +2,7 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(x)
+<<<<<<< HEAD
   if x % 3 == 0 && x % 5 == 0
     return "FizzBuzz"
   elsif x % 3 == 0
@@ -12,3 +13,14 @@ def fizzbuzz(x)
     return nil
   end
 end
+=======
+  if x % 3
+    return "Fizz"
+  elsif x % 5
+    return "Buzz"
+  elsif x % 3 && 5
+    return "Fizz"+"Buzz"
+  else
+    return nil
+  end
+>>>>>>> 2494a4988355a67a31b6b0c8e94996069206ad45
